b09fe829912aecdb8fb696d42da26c788fac5ee2
vigz <kyle.vigorito@gmail.com>
2019-11-21 11:35:05 -0500

    added scaffolding for index.js with redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import ReactDOM from 'react-dom'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import followingBarReducer from './reducers/followingBarReducer';
+import streamsReducer from './reducers/streamsReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './App.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+const rootReducer = combineReducers({ followingBarReducer, streamsReducer })
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}><App className='app-container'/></Provider>, document.getElementById('root'));
+registerServiceWorker();
