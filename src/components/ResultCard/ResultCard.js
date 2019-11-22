7c366b9d151268842ab14851e5487418dca71711
vigz <kyle.vigorito@gmail.com>
2019-11-21 20:08:53 -0500

    have data passing through and display correctly, styling inc

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,15 +1,24 @@
 import React from 'react'
 
+import './ResultCard.scss';
+
 const ResultCard = (props) => {
 return (
   <div className="card resultCard">
-    {props.name}
-    <img src={props.image_url} class="card-img-top" alt="card_image"/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+    <a href={props.url} className="resultLink" target="_blank">
+      <img src={props.imageUrl} className="card-image" alt="card_image"/>
+      <div className="card-body">
+        <h5 className="card-name">{props.name}</h5>
+        <p className="card-text">{props.rating}/5</p>
+        <p className="card-text">{props.price}</p>
+        <p className="card-text">{props.phone}</p>
+        <p className="card-text">{props.location.address1}</p>
+        <p className="card-text">{props.location.city}</p>
+        <p className="card-text">{props.location.zip_code}</p>
+        <p className="card-text">{props.location.country}</p>
+        <p className="card-text">{props.location.state}</p>
+      </div>
+    </a>
   </div>
 )
 }
--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -3,7 +3,14 @@ import ResultCard from '../ResultCard/ResultCard.js'
 
 const ResultsList = (props) => {
   const resultCards = props.resultsList.map((result)=>{
-    return <ResultCard name={result.name}/>
+    return <ResultCard
+      name={result.name}
+      imageUrl={result.image_url}
+      url={result.url}
+      phone={result.display_phone}
+      rating={result.rating}
+      price={result.price}
+      location={result.location}/>
   })
 return (
   <div className="resultsList">
