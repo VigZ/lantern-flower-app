440b0f69c0636c3c748faa4b453d429a63a97310
vigz <kyle.vigorito@gmail.com>
2019-11-22 09:12:15 -0500

    Updates

---
+++ precache-manifest.08c632ccbbb4baf43f119a9a3dcaf3ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0542af62f4443118af58f34a2b311c9a",
+    "url": "/lantern-flower-app/index.html"
+  },
+  {
+    "revision": "689e88c4a5df9f9bcf7e",
+    "url": "/lantern-flower-app/static/css/main.f151bf55.chunk.css"
+  },
+  {
+    "revision": "579de4e32f0eacd4b1ad",
+    "url": "/lantern-flower-app/static/js/2.5e3a875a.chunk.js"
+  },
+  {
+    "revision": "689e88c4a5df9f9bcf7e",
+    "url": "/lantern-flower-app/static/js/main.6c1cc542.chunk.js"
+  },
+  {
+    "revision": "8e8c452f24b79ecdecbe",
+    "url": "/lantern-flower-app/static/js/runtime-main.ef82601f.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/lantern-flower-app/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ac86e02fb7a96c154651fedfab6afa47",
+    "url": "/lantern-flower-app/static/media/flower-banner.ac86e02f.png"
+  },
+  {
+    "revision": "ff56fc2d5760d750673dbbbe1e33f3f1",
+    "url": "/lantern-flower-app/static/media/flower-marker.ff56fc2d.png"
+  },
+  {
+    "revision": "92ba56c922d3dbb0088791c5a633c936",
+    "url": "/lantern-flower-app/static/media/green-semi-flower.92ba56c9.png"
+  }
+]);
